0fd54fc57839c1416ecdd12088273234cf00c68b
Quoc Nguyen 19 <nguyenhuuquoc19@gmail.com>
2025-02-18 22:18:12 +0700

    ECM-0001_Init-new-project

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// Init middleware
+app.use(morgan("dev")); //combined/common/short/tiny
+app.use(helmet());
+app.use(compression());
+
+// Init Database
+// Init Router
+app.get("/", ( req, res, next) => {
+    const test = "This is a text";
+    return res.status(200).json({
+        message: "Hello world 2!",
+        metadata: test.repeat(200000),
+    });
+});
+
+// Handling Errors
+
+module.exports = app
